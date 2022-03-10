65bafa722119c1dab7777e8cb827c045422db587
Tchimwa Sougang <tsougang@outlook.com>
2022-03-10 02:32:39 -0500

    Update the web server code !

--- scripts/webserver.sh
+++ scripts/webserver.sh
@@ -8,13 +8,12 @@ systemctl start apache2
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Networking DU</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
   <!-- BEGIN -->
-  <center><img src="http://placekitten.com/640/480"></img></center>
-  <center><h2>Meow World!</h2></center>
-  <center>Welcome to the HashiCat application. Meow! =^._.^=</center>
+    <center><h2>Fellow Network Engineers </h2></center>
+  <center>Welcome to the LABTIME Session 3!=^._.^=</center>
   <!-- END -->
   
   </div>
