1593c448fcba7b67ec2380e501071d12199d2d07
Tchimwa Sougang <56332566+Tchimwa@users.noreply.github.com>
2022-03-15 12:00:45 -0400

    Update main.tf
    Deleted the cred

--- main.tf
+++ main.tf
@@ -10,10 +10,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "10d33b0d-f927-45f4-a5a2-dd2ef6a282eb"
-  client_id = "f39e37ef-9337-4ad4-b985-36aabd6716bf"
-  client_secret = "rydrKrAnz9WudNnAaiHoH3KXSX-G09lTyo"
-  tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
 }
 
 resource "azurerm_resource_group" "main" {
